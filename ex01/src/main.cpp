700acde52c366c6c08cb3a3da52c37deaf1f0521
mariocaro13 <mariocaro2002@gmail.com>
2026-01-14 00:22:34 +0100

    Refactor main function to enhance ScavTrap testing and improve attack and repair sequences

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mcaro-ro <mcaro-ro@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 21:17:27 by mcaro-ro          #+#    #+#             */
-/*   Updated: 2026/01/13 23:04:13 by mcaro-ro         ###   ########.fr       */
+/*   Updated: 2026/01/14 00:22:15 by mcaro-ro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,68 @@
 
 int main(void)
 {
-	ScavTrap	serena("Serena");
+	std::cout << "\n----- CONSTRUCTION TEST -----\n"<< std::endl;
+	ClapTrap	dummy("Dummy");
 	ClapTrap	bob("Bob");
+	ScavTrap	serena("Serena");
+
+	dummy.setAttackDamage(0);
+	dummy.setHitPoints(10000);
+	dummy.setEnergyPoints(10000);
+
+	std::cout << "\n----- BASICS TEST -----"<< std::endl;
 
 	std::cout << "\n--- Normal Attacks ---"<< std::endl;
-	serena.attack("Bob");
-	bob.takeDamage(serena.getAttackDamage());
+	bob.attack("Dummy");
+	dummy.takeDamage(bob.getAttackDamage());
+	std::cout << std::endl;
+
+	serena.attack("Dummy");
+	dummy.takeDamage(serena.getAttackDamage());
+
+	std::cout << "\n--- Normal Repairing ---"<< std::endl;
+	bob.beRepaired(ClapTrap::defaultHitPoints);
+	serena.beRepaired(ScavTrap::defaultHitPoints);
+
+	std::cout << "\n--- Lethal damage ---"<< std::endl;
+	bob.takeDamage(bob.getHitPoints() + 10);
+	serena.takeDamage(serena.getHitPoints() + 10);
+
+	std::cout << "\n--- Try to act while dead ---"<< std::endl;
+	bob.attack("Dummy");
+	serena.attack("Dummy");
+
+	std::cout << "\n--- Spends all energy ---"<< std::endl;
+
+	bob.setHitPoints(ClapTrap::defaultHitPoints);
+	bob.setEnergyPoints(ClapTrap::defaultEnergyPoints);
+	while (bob.getEnergyPoints() > 0)
+	{
+		bob.attack("Dummy");
+	}
+	std::cout << std::endl;
+
+	serena.setHitPoints(ScavTrap::defaultHitPoints);
+	serena.setEnergyPoints(ScavTrap::defaultEnergyPoints);
+	while (serena.getEnergyPoints() > 0)
+	{
+		serena.attack("Dummy");
+	}
+	
+	std::cout << "\n--- Try to act with no energy ---"<< std::endl;
+	bob.attack("Dummy");
+	bob.beRepaired(5);
+	std::cout << std::endl;
+
+	serena.attack("Dummy");
+	serena.beRepaired(5);
+
+	std::cout << "\n----- SCAVTRAP TEST -----"<< std::endl;
 
 	std::cout << "\n--- ScavTrap special ability ---"<< std::endl;
 	serena.guardGate();
 
-	std::cout << "\n--- Normal Repairing ---"<< std::endl;
-	serena.beRepaired(5);
-
-	std::cout << "\n--- Bob takes lethal damage ---"<< std::endl;
-	bob.takeDamage(bob.getHitPoints() + 10);
-
-	std::cout << "\n--- Bob tries to act while dead ---"<< std::endl;
-	bob.attack("Serena");
-	bob.beRepaired(5);
-
-	std::cout << "\n--- Serena spends all her energy ---"<< std::endl;
-	while (serena.getEnergyPoints() > 0)
-	{
-		std::cout << "Serena's energy points: " << serena.getEnergyPoints() << std::endl;
-		serena.attack("Bob");
-	}
-
-	std::cout << "\n--- Serena tries to act with no energy ---"<< std::endl;
-	serena.attack("Bob");
-	serena.beRepaired(5);
-
-	std::cout << std::endl;
+	std::cout << "\n----- End of simulation -----"<< std::endl;
 
 	return (0);
 }
